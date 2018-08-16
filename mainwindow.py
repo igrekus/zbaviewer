a5fb56ce3dc10c89fe4395d43f59b404487efc57
Unknown <upload234@mail.ru>
2018-08-16 18:04:02 +0300

    Pyparsing test

--- mainwindow.py
+++ mainwindow.py
@@ -1,82 +1,82 @@
-import copy
-from zbatfield import ZbaTfield
-from zbaufield import ZbaUfield
-from zbarect import ZbaRect
-from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QGraphicsScene, QGraphicsRectItem
-from PyQt5.QtGui import QBrush, QColor
-from PyQt5.QtCore import Qt
-
-
-class MainWindow(QMainWindow):
-
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-
-        self.setAttribute(Qt.WA_QuitOnClose)
-        self.setAttribute(Qt.WA_DeleteOnClose)
-
-        # create instance variables
-        # ui
-        self.ui = uic.loadUi("mainwindow.ui", self)
-
-        # scene
-        self.scene = QGraphicsScene(self)
-
-        # origin
-        self.originRect = QGraphicsRectItem(0, 0, 4, 4)
-        self.originRect.setPos(-2, -2)
-        self.scene.addItem(self.originRect)
-
-        # setup signals
-        self.ui.btnZoomIn.clicked.connect(self.onBtnZoomInClicked)
-        self.ui.btnZoomOut.clicked.connect(self.onBtnZoomOutClicked)
-
-    def initApp(self):
-        self.ui.graphicsHudView.setScene(self.scene)
-        self.ui.graphicsHudView.setSceneRect(0, 0, 2000000, 2000000)
-        self.ui.graphicsHudView.centerOn(self.originRect)
-        # self.ui.graphicsHudView.initView()
-
-        # self.ui.graphicsHudView.verticalScrollBar().valueChanged.connect(self.onHudViewScroll)
-        # self.ui.graphicsHudView.verticalScrollBar().sliderMoved.connect(self.onHudViewScroll)
-        # self.ui.graphicsHudView.horizontalScrollBar().valueChanged.connect(self.onHudViewScroll)
-
-    def resizeEvent(self, event):
-        self.ui.graphicsHudView.resize(self.ui.graphicsHudView.size())
-        self.ui.graphicsHudView.hudOverlayScene.setSceneRect(
-            self.ui.graphicsHudView.hudOverlayScene.items()[0].boundingRect())
-        super(MainWindow, self).resizeEvent(event)
-
-    def onHudViewScroll(self, int):
-        # self.ui.graphicsHudView.hudOverlayScene.items()[0].update(0, 0, 50, 50)
-        # self.ui.graphicsHudView.hudOverlayScene.update()
-        pass
-
-    def onBtnZoomInClicked(self):
-        self.ui.graphicsHudView.centerOn(0, 0)
-        print("zoom in")
-
-    def onBtnZoomOutClicked(self):
-        print("zoom out")
-
-    def decodeUfield(self, tfpos: list, uf: ZbaUfield, scale: int):
-        self.ui.graphicsHudView.hudLegendItem.zoom_scale = scale
-        for p in uf.pos_list:
-            for r in uf.rect_list:
-                tr = ZbaRect.fromCopy(r)
-                tr.setBrush(QBrush(QColor(Qt.gray)))
-                tr.scaleRect(scale)
-                tr.setPos((tfpos[0] + p[0])*scale + tr.posx,
-                          -(tfpos[1] + p[1])*scale - tr.posy - tr.rect().height())
-                self.scene.addItem(tr)
-
-    def decodeTfield(self, tf: ZbaTfield, scale: int):
-        self.ui.graphicsHudView.hudLegendItem.zoom_scale = scale
-        for p in tf.pos_list:
-            for u in tf.ufield_list:
-                self.decodeUfield(p, u, scale)
-        # for u in tf.ufield_list:
-        #     print(u)
-        #     for r in u.rect_list:
-        #         print(r)
+# import copy
+# from zbatfield import ZbaTfield
+# from zbaufield import ZbaUfield
+# from zbarect import ZbaRect
+# # from PyQt5 import uic
+# # from PyQt5.QtWidgets import QMainWindow, QGraphicsScene, QGraphicsRectItem
+# # from PyQt5.QtGui import QBrush, QColor
+# # from PyQt5.QtCore import Qt
+#
+#
+# class MainWindow(QMainWindow):
+#
+#     def __init__(self, parent=None):
+#         super(MainWindow, self).__init__(parent)
+#
+#         self.setAttribute(Qt.WA_QuitOnClose)
+#         self.setAttribute(Qt.WA_DeleteOnClose)
+#
+#         # create instance variables
+#         # ui
+#         self.ui = uic.loadUi("mainwindow.ui", self)
+#
+#         # scene
+#         self.scene = QGraphicsScene(self)
+#
+#         # origin
+#         self.originRect = QGraphicsRectItem(0, 0, 4, 4)
+#         self.originRect.setPos(-2, -2)
+#         self.scene.addItem(self.originRect)
+#
+#         # setup signals
+#         self.ui.btnZoomIn.clicked.connect(self.onBtnZoomInClicked)
+#         self.ui.btnZoomOut.clicked.connect(self.onBtnZoomOutClicked)
+#
+#     def initApp(self):
+#         self.ui.graphicsHudView.setScene(self.scene)
+#         self.ui.graphicsHudView.setSceneRect(0, 0, 2000000, 2000000)
+#         self.ui.graphicsHudView.centerOn(self.originRect)
+#         # self.ui.graphicsHudView.initView()
+#
+#         # self.ui.graphicsHudView.verticalScrollBar().valueChanged.connect(self.onHudViewScroll)
+#         # self.ui.graphicsHudView.verticalScrollBar().sliderMoved.connect(self.onHudViewScroll)
+#         # self.ui.graphicsHudView.horizontalScrollBar().valueChanged.connect(self.onHudViewScroll)
+#
+#     def resizeEvent(self, event):
+#         self.ui.graphicsHudView.resize(self.ui.graphicsHudView.size())
+#         self.ui.graphicsHudView.hudOverlayScene.setSceneRect(
+#             self.ui.graphicsHudView.hudOverlayScene.items()[0].boundingRect())
+#         super(MainWindow, self).resizeEvent(event)
+#
+#     def onHudViewScroll(self, int):
+#         # self.ui.graphicsHudView.hudOverlayScene.items()[0].update(0, 0, 50, 50)
+#         # self.ui.graphicsHudView.hudOverlayScene.update()
+#         pass
+#
+#     def onBtnZoomInClicked(self):
+#         self.ui.graphicsHudView.centerOn(0, 0)
+#         print("zoom in")
+#
+#     def onBtnZoomOutClicked(self):
+#         print("zoom out")
+#
+#     def decodeUfield(self, tfpos: list, uf: ZbaUfield, scale: int):
+#         self.ui.graphicsHudView.hudLegendItem.zoom_scale = scale
+#         for p in uf.pos_list:
+#             for r in uf.rect_list:
+#                 tr = ZbaRect.fromCopy(r)
+#                 tr.setBrush(QBrush(QColor(Qt.gray)))
+#                 tr.scaleRect(scale)
+#                 tr.setPos((tfpos[0] + p[0])*scale + tr.posx,
+#                           -(tfpos[1] + p[1])*scale - tr.posy - tr.rect().height())
+#                 self.scene.addItem(tr)
+#
+#     def decodeTfield(self, tf: ZbaTfield, scale: int):
+#         self.ui.graphicsHudView.hudLegendItem.zoom_scale = scale
+#         for p in tf.pos_list:
+#             for u in tf.ufield_list:
+#                 self.decodeUfield(p, u, scale)
+#         # for u in tf.ufield_list:
+#         #     print(u)
+#         #     for r in u.rect_list:
+#         #         print(r)
--- zbarect.py
+++ zbarect.py
@@ -1,8 +1,8 @@
 import re
-from PyQt5.QtWidgets import QGraphicsRectItem
+# from PyQt5.QtWidgets import QGraphicsRectItem
 
 
-class ZbaRect(QGraphicsRectItem):
+class ZbaRect:
     """
     ZBA rectangle class.
     
@@ -15,7 +15,6 @@ class ZbaRect(QGraphicsRectItem):
     """
 
     def __init__(self, x: float=0, y: float=0, w: float=0, h: float=0, d: int=0, parent=None):
-        super(ZbaRect, self).__init__(parent)
         """
         Default constructor, takes:
         :param x: float 
@@ -24,48 +23,49 @@ class ZbaRect(QGraphicsRectItem):
         :param h: float         
         :param d: int, [0-7]
         """
-        self.setRect(0, 0, w, h)
-        self.posx: float = x
-        self.posy: float = y
-        self.dose_id: int = d
+        print("new zbarect")
+        # self.setRect(0, 0, w, h)
+        # self.posx: float = x
+        # self.posy: float = y
+        # self.dose_id: int = d
 
-    def __str__(self):
-        return "ZBARect(rect:" + str(self.posx) + ", " + str(self.posy) + ", " + \
-               str(self.rect().width()) + ", " + str(self.rect().height()) + \
-               ", dose id:" + str(self.dose_id) + ")"
-
-    def scaleRect(self, scale):
-        self.posx *= scale
-        self.posy *= scale
-        rect = self.rect()
-        rect.adjust(0, 0, rect.width() * scale, rect.height() * scale)
-        self.setRect(rect)
-
-    @classmethod
-    def fromCopy(cls, src):
-        return cls(src.posx, src.posy, src.rect().width(), src.rect().height(), src.dose_id)
-
-    @classmethod
-    def from_string(cls, rect_as_string: str):
-        """
-        Makes ZbaRect instance object from a given sanitized string.
-        :param rect_as_string: "Rx,y,w,h,*[1-8];"
-        :return: ZbaRect instance object
-        """
-        # (regex matches both R forms: Rfloat,float,float,float; | Rfloat,float,float,float,*[1-8];)
-        p = re.compile(r"^R\d*?\.?\d+?,\d*?\.?\d+?,\d*?\.?\d+?,\d*?\.?\d+?(,\*[0-7])?;$")
-        if not p.match(rect_as_string):
-            raise ValueError("Wrong rect string format:", rect_as_string)
-
-        tmp = rect_as_string.strip("R").strip(";").split(",")
-
-        if len(tmp) == 4:
-            return cls(float(tmp[0]), float(tmp[1]), float(tmp[2]), float(tmp[3]), 0)
-        elif len(tmp) == 5:
-            dose = int(tmp[4].strip("*"))
-            if dose not in range(8):
-                raise ValueError("Wrong dose specifier:", dose)
-
-            return cls(float(tmp[0]), float(tmp[1]), float(tmp[2]), float(tmp[3]), dose)
-        else:
-            raise ValueError("Wrong rect parameter list:", tmp)
+    # def __str__(self):
+    #     return "ZBARect(rect:" + str(self.posx) + ", " + str(self.posy) + ", " + \
+    #            str(self.rect().width()) + ", " + str(self.rect().height()) + \
+    #            ", dose id:" + str(self.dose_id) + ")"
+    #
+    # def scaleRect(self, scale):
+    #     self.posx *= scale
+    #     self.posy *= scale
+    #     rect = self.rect()
+    #     rect.adjust(0, 0, rect.width() * scale, rect.height() * scale)
+    #     self.setRect(rect)
+    #
+    # @classmethod
+    # def fromCopy(cls, src):
+    #     return cls(src.posx, src.posy, src.rect().width(), src.rect().height(), src.dose_id)
+    #
+    # @classmethod
+    # def from_string(cls, rect_as_string: str):
+    #     """
+    #     Makes ZbaRect instance object from a given sanitized string.
+    #     :param rect_as_string: "Rx,y,w,h,*[1-8];"
+    #     :return: ZbaRect instance object
+    #     """
+    #     # (regex matches both R forms: Rfloat,float,float,float; | Rfloat,float,float,float,*[1-8];)
+    #     p = re.compile(r"^R\d*?\.?\d+?,\d*?\.?\d+?,\d*?\.?\d+?,\d*?\.?\d+?(,\*[0-7])?;$")
+    #     if not p.match(rect_as_string):
+    #         raise ValueError("Wrong rect string format:", rect_as_string)
+    #
+    #     tmp = rect_as_string.strip("R").strip(";").split(",")
+    #
+    #     if len(tmp) == 4:
+    #         return cls(float(tmp[0]), float(tmp[1]), float(tmp[2]), float(tmp[3]), 0)
+    #     elif len(tmp) == 5:
+    #         dose = int(tmp[4].strip("*"))
+    #         if dose not in range(8):
+    #             raise ValueError("Wrong dose specifier:", dose)
+    #
+    #         return cls(float(tmp[0]), float(tmp[1]), float(tmp[2]), float(tmp[3]), dose)
+    #     else:
+    #         raise ValueError("Wrong rect parameter list:", tmp)
